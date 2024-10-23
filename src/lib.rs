8d338becd9f265f876502a3b41af1474b44aa733
brent <bpederse@gmail.com>
2024-10-23 08:57:27 -0700

    use rust_htslib

--- src/lib.rs
+++ src/lib.rs
@@ -1,322 +1,181 @@
-use indexmap::IndexMap;
-pub use noodles::bcf;
-use noodles::bgzf;
-use noodles::core::{Position, Region};
-pub use noodles::csi;
-pub use noodles::tabix;
-use noodles::vcf;
-use noodles::vcf::header::record::value::map::Contig;
-use noodles::vcf::header::record::value::Map;
+use rust_htslib::bcf::{self, Read};
+use std::{fmt, io, path::Path};
 
-pub mod detect;
-use detect::{Compression, Format};
-use noodles::vcf::variant::Record;
-use std::io::{self, BufRead, BufReader};
-use std::io::{Read, Seek};
-
-pub trait VariantReader {
-    fn next_record(&mut self, header: &vcf::Header, v: &mut vcf::Record) -> io::Result<usize>;
+pub enum ReaderInner {
+    Indexed(bcf::IndexedReader),
+    Plain(bcf::Reader),
 }
 
-impl<R> VariantReader for vcf::io::Reader<R>
-where
-    R: BufRead,
-{
-    fn next_record(&mut self, header: &vcf::Header, v: &mut vcf::Record) -> io::Result<usize> {
-        self.read_record(v)
-    }
-}
-impl<R> VariantReader for bcf::io::Reader<R>
-where
-    R: BufRead,
-{
-    fn next_record(&mut self, header: &vcf::Header, v: &mut vcf::Record) -> io::Result<usize> {
-        self.read_record(v)
-    }
+pub struct Reader<'a> {
+    inner: ReaderInner,
+    header: bcf::header::HeaderView,
+    records: Option<bcf::Records<'a, bcf::Reader>>,
+    current_record: Option<bcf::Record>,
 }
 
-pub enum XCF<R> {
-    Vcf(Box<dyn VariantReader>),
-    IndexedVcf(vcf::io::IndexedReader<bgzf::Reader<BufReader<Box<R>>>>),
-    IndexedBcf(bcf::io::IndexedReader<bgzf::Reader<BufReader<Box<R>>>>),
-}
-
-pub struct Reader<R> {
-    inner: XCF<R>,
-    header: vcf::Header,
-    variant: Option<vcf::Record>,
-}
-
-impl<R> Reader<R>
-where
-    R: Read + 'static,
-{
-    pub fn new(inner: XCF<R>, header: vcf::Header) -> Self {
+impl<'a> Reader<'a> {
+    pub fn new(inner: ReaderInner) -> Self {
+        let header = match &inner {
+            ReaderInner::Indexed(r) => r.header().clone(),
+            ReaderInner::Plain(r) => r.header().clone(),
+        };
         Self {
             inner,
             header,
-            variant: None,
+            records: None,
+            current_record: None,
         }
     }
 
-    pub fn take(&mut self) -> Option<vcf::Record> {
-        self.variant.take()
+    pub fn take(&mut self) -> Option<bcf::Record> {
+        self.current_record.take()
     }
 
-    pub fn next_record(&mut self, header: &vcf::Header, v: &mut vcf::Record) -> io::Result<usize> {
-        // if self.variant is set, then use that to set v
-        if let Some(variant) = self.variant.take() {
-            *v = variant;
-            return Ok(1);
+    pub fn next_record(&mut self) -> io::Result<Option<bcf::Record>> {
+        if let Some(record) = self.current_record.take() {
+            return Ok(Some(record));
         }
+
         match &mut self.inner {
-            XCF::Vcf(reader) => reader.next_record(header, v),
-            XCF::IndexedVcf(reader) => reader.read_record(v),
-            XCF::IndexedBcf(reader) => reader.read_record(v),
+            ReaderInner::Indexed(reader) => {
+                let mut record = reader.empty_record();
+                if let Some(records) = self.records.as_mut() {
+                    return records
+                        .next()
+                        .transpose()
+                        .map_err(|e| io::Error::new(io::ErrorKind::Other, e));
+                }
+                match reader.read(&mut record) {
+                    Some(Ok(())) => Ok(Some(record)),
+                    Some(Err(e)) => Err(io::Error::new(io::ErrorKind::Other, e)),
+                    None => Ok(None),
+                }
+            }
+            ReaderInner::Plain(reader) => match reader.records().next() {
+                Some(Ok(record)) => Ok(Some(record)),
+                Some(Err(e)) => Err(io::Error::new(io::ErrorKind::Other, e)),
+                None => Ok(None),
+            },
         }
     }
 
-    pub fn from_reader(reader: Box<R>, path: Option<&str>) -> io::Result<Reader<R>> {
-        let mut reader = BufReader::new(reader);
-        let compression = detect::detect_compression(&mut reader)?;
-        let format = detect::detect_format(&mut reader, compression)?;
-        let csi = find_index(path);
-
-        Ok(match (format, compression, csi /*, seekable */) {
-            (Format::Vcf, None, _) => {
-                let mut reader = vcf::io::Reader::new(reader);
-                let header = reader.read_header()?;
-                Reader::new(XCF::Vcf(Box::new(reader)), header)
+    pub fn from_path<P: AsRef<Path>>(path: P) -> io::Result<Reader<'static>> {
+        // Try indexed reader first
+        match bcf::IndexedReader::from_path(&path) {
+            Ok(indexed_reader) => Ok(Reader::new(ReaderInner::Indexed(indexed_reader))),
+            Err(_) => {
+                // Fall back to plain reader
+                let reader = bcf::Reader::from_path(path)
+                    .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+                Ok(Reader::new(ReaderInner::Plain(reader)))
             }
-            (Format::Vcf, Some(Compression::Bgzf), Some(csi)) => {
-                let mut reader = vcf::io::IndexedReader::new(reader, csi);
-                let header = reader.read_header()?;
-                Reader::new(XCF::IndexedVcf(reader), header)
-            }
-            (Format::Vcf, Some(Compression::Bgzf), None) => {
-                let bgzf_reader = bgzf::Reader::new(reader);
-                let mut reader = vcf::io::Reader::new(bgzf_reader);
-                let header = reader.read_header()?;
-                Reader::new(XCF::Vcf(Box::new(reader)), header)
-            }
-
-            (Format::Bcf, Some(Compression::Bgzf), Some(csi)) => {
-                //let bgzf_reader = bgzf::Reader::new(reader);
-                let mut reader = bcf::io::IndexedReader::new(reader, csi);
-                let header = reader.read_header()?;
-                Reader::new(XCF::IndexedBcf(reader), header)
-            }
-            (Format::Bcf, _, _) => {
-                let mut reader = bcf::io::Reader::new(reader);
-                let header = reader.read_header()?;
-                Reader::new(XCF::Vcf(Box::new(reader)), header)
-            }
-        })
+        }
     }
-    pub fn header(&self) -> &vcf::Header {
+
+    pub fn header(&self) -> &bcf::header::HeaderView {
         &self.header
     }
 }
 
-// if the vcf header has the correct info, we check that the contig is present and
-// the the region is not beyond the end of the contig.
-// if the header does not have this info, then we do not check the contig.
-fn check_contig(contigs: &IndexMap<String, Map<Contig>>, region: &Region) -> io::Result<usize> {
-    let contig_i = if !contigs.is_empty() {
-        match contigs.get_index_of(region.name()) {
-            None => {
-                return Err(io::Error::new(
-                    io::ErrorKind::InvalidInput,
-                    format!("unknown contig: {}", region.name()),
-                ))
-            }
-            Some(i) => {
-                let contig = contigs.get_index(i).unwrap().1;
-                if let Some(len) = contig.length() {
-                    if len < usize::from(region.interval().start().unwrap_or(Position::MAX)) {
-                        return Err(io::Error::new(
-                            io::ErrorKind::InvalidInput,
-                            format!("region {} is out of bounds for contig {:?}", region, contig),
-                        ));
-                    }
-                }
-
-                i
-            }
-        }
-    } else {
-        usize::MAX
-    };
-    Ok(contig_i)
-}
-
-fn simple_skip<R: Read + 'static>(
-    reader: &mut Reader<R>,
-    header: &vcf::Header,
-    region: &Region,
-) -> io::Result<()> {
-    let mut v = vcf::Record::default();
-    let one = Position::try_from(1).unwrap();
-    let start =
-        noodles::core::Position::try_from(usize::from(region.interval().start().unwrap_or(one)))
-            .unwrap();
-
-    let contigs = header.contigs();
-    let contig_i = check_contig(contigs, region)?; // check that the contig is valid (if it exists)
-
-    let mut last_chrom: Option<String> = None;
-
-    loop {
-        reader.next_record(header, &mut v)?;
-        let end = match v.variant_end(header) {
-            Ok(p) => p,
-            _ => {
-                let s = v
-                    .variant_start()
-                    .unwrap_or(Ok(Position::MAX))
-                    .unwrap_or(Position::MAX);
-                if s == Position::MAX {
-                    continue;
-                }
-                s.checked_add(1).unwrap_or(Position::MAX)
-            }
-        };
-        // TODO: not sure about 1-based vs 0-based in noodles. might need end > start
-        if end >= start && chrom_equals(v.reference_sequence_name(), region.name()) {
-            reader.variant = Some(v);
-            break;
-        }
-
-        if contig_i == usize::MAX {
-            continue;
-        }
-        // we check that user isn't requesting a contig that is before the current one.
-        if let Some(ilast_chrom) = &last_chrom {
-            // if we have just seen this chromosome, then we can skip the check
-            if ilast_chrom == v.reference_sequence_name() {
-                continue;
-            }
-            match contigs.get_index_of(v.reference_sequence_name()) {
-                None => continue,
-                Some(i) => {
-                    if i > contig_i {
-                        // return an error about ordering of chromosomes
-                        return Err(io::Error::new(
-                            io::ErrorKind::InvalidInput,
-                            format!(
-                                "contig {} is out of order relative to {}",
-                                v.reference_sequence_name(),
-                                region.name()
-                            ),
-                        ));
-                    }
-                }
-            }
-            last_chrom = Some(v.reference_sequence_name().to_string());
-        } else {
-            last_chrom = Some(v.reference_sequence_name().to_string());
-        }
-    }
-    Ok(())
-}
-
 pub trait Skip {
-    fn skip_to(&mut self, header: &vcf::Header, region: &Region) -> io::Result<()>;
+    fn skip_to(&mut self, region: &str) -> io::Result<()>;
 }
 
-impl<R> Skip for Reader<R>
-where
-    R: Read + Seek + 'static,
-{
-    fn skip_to(&mut self, header: &vcf::Header, region: &Region) -> io::Result<()> {
-        match &mut self.inner {
-            XCF::IndexedVcf(reader) => {
-                let mut q = reader.query(header, region)?;
-                self.variant = match q.next() {
-                    Some(Ok(v)) => Some(v),
-                    Some(Err(e)) => return Err(e),
-                    None => None,
-                };
-                Ok(())
-            }
-            XCF::IndexedBcf(reader) => {
-                let mut q = reader.query(header, region)?;
-                self.variant = match q.next() {
-                    Some(Ok(v)) => Some(v),
-                    Some(Err(e)) => return Err(e),
-                    None => None,
-                };
-                Ok(())
-            }
-
-            _ => simple_skip(self, header, region),
+impl<'a> Skip for Reader<'a> {
+    fn skip_to(&mut self, region: &str) -> io::Result<()> {
+        // Parse region string (e.g., "chr1:2000")
+        let parts: Vec<&str> = region.split(':').collect();
+        if parts.len() != 2 {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "Invalid region format",
+            ));
         }
-    }
-}
 
-#[inline]
-fn chrom_equals(s: &str, other: &(impl AsRef<[u8]> + ?Sized)) -> bool {
-    s.as_bytes() == other.as_ref()
-}
+        let chrom = parts[0];
+        let pos: u64 = parts[1]
+            .parse()
+            .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
 
-fn find_index(path: Option<&str>) -> Option<csi::Index> {
-    if let Some(path) = path {
-        let mut index_path = path.to_string();
-        // TODO: look for csi or .tbi or use ##idx## in path.
-        index_path.push_str(".csi");
-        csi::read(index_path)
-            .or_else(|_| {
-                let mut index_path = path.to_string();
-                index_path.push_str(".tbi");
-                tabix::read(index_path)
-            })
-            .ok()
-    } else {
-        None
+        match &mut self.inner {
+            ReaderInner::Indexed(reader) => {
+                let rid = reader
+                    .header()
+                    .name2rid(chrom.as_bytes())
+                    .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
+                reader
+                    .fetch(rid, pos - 1, None)
+                    .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+            }
+            ReaderInner::Plain(reader) => {
+                let target_rid = reader
+                    .header()
+                    .name2rid(chrom.as_bytes())
+                    .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
+                // Scan through records until we find one that's >= our target position
+                match reader.records().next() {
+                    Some(Ok(record)) => {
+                        let record_rid = record.rid().unwrap();
+
+                        if record_rid > target_rid {
+                            // We've gone past our target chromosome
+                            self.current_record = Some(record);
+                            return Ok(());
+                        } else if record_rid == target_rid {
+                            if (record.end() as u64) >= pos {
+                                // Found a record at or after our target position
+                                self.current_record = Some(record);
+                                return Ok(());
+                            }
+                        }
+                        // Otherwise continue scanning
+                    }
+                    Some(Err(e)) => return Err(io::Error::new(io::ErrorKind::Other, e)),
+                    None => return Ok(()),
+                }
+                Ok(()) // Reached end of file without finding matching position
+            }
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use noodles::core::{Position, Region};
-
     use super::*;
-    use std::io::Cursor;
+    use std::path::PathBuf;
 
     #[test]
     fn test_read_vcf() {
-        let vcf_data = b"\
-            ##fileformat=VCFv4.2\n\
-            #CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n\
-            chr1\t1000\t.\tA\tC\t.\t.\t.\n\
-            chr1\t2000\t.\tG\tT\t.\t.\t.\n\
-            chr2\t3000\t.\tC\tG\t.\t.\t.\n\
-        ";
-        let _cursor = Cursor::new(vcf_data);
-        let path = "tests/t.vcf.gz";
-        let rdr = BufReader::new(std::fs::File::open(&path).unwrap());
-        let rdr = _cursor;
-        let rdr = Box::new(rdr);
-
-        let mut reader = Reader::from_reader(rdr, Some(path)).expect("error creating new reader");
-        let header = reader.header().clone();
-
-        let start = Position::try_from(2000).expect("error creating start");
-        let region = Region::new("chr1", start..);
+        let path = PathBuf::from("tests/t.vcf.gz");
+        let mut reader = Reader::from_path(&path).expect("error creating new reader");
+        let _header = reader.header().clone();
 
         reader
-            .skip_to(&header, &region)
+            .skip_to("chr1:2000")
             .expect("error skipping to region");
 
-        let mut v = vcf::Record::default();
+        // Store the record result in a separate variable to end the borrow
+        let record = reader.next_record().expect("error reading record");
+        if let Some(record) = record {
+            assert_eq!(record.pos(), 1999); // 0-based position
+            let chrom = reader.header().rid2name(record.rid().unwrap()).unwrap();
+            let chrom_str = std::str::from_utf8(chrom).unwrap();
+            println!("Record: {:?}", record);
+            println!("Record: {}:{}", chrom_str, record.pos());
+        } else {
+            panic!("No record found");
+        }
 
+        // now if we skip backwards we should NOT get the same record
         reader
-            .next_record(&header, &mut v)
-            .expect("error reading record");
-        assert_eq!(
-            v.variant_start().unwrap().unwrap(),
-            Position::try_from(2000).unwrap()
-        );
-        eprintln!("v: {:?}", v);
+            .skip_to("chr1:1999")
+            .expect("error skipping to region");
+        let r = reader.next_record().expect("error reading record").unwrap();
+        let chrom = reader.header().rid2name(r.rid().unwrap()).unwrap();
+        let chrom_str = std::str::from_utf8(chrom).unwrap();
+        eprintln!("Record: {}:{}", chrom_str, r.pos());
+        assert!(r.pos() != 1999);
     }
 }
